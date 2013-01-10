829cca09987880135e90d82dd4fd5f0b21926608
Chris Zelenak <netshade@gmail.com>
2013-01-10 15:11:36 -0500

    initial commit

---
+++ lib/metriks-instrumental.rb
@@ -0,0 +1,7 @@
+require "metriks-instrumental/version"
+
+module Metriks
+  module Instrumental
+    # Your code goes here...
+  end
+end
---
+++ lib/metriks-instrumental/version.rb
@@ -0,0 +1,5 @@
+module Metriks
+  module Instrumental
+    VERSION = "0.0.1"
+  end
+end
